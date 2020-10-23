086d65fd80452bdc20d30971cea9becd9a210f08
david810376 <33507197+david810376@users.noreply.github.com>
2020-10-23 14:53:53 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,36 @@
-(function(window) {
-  'use strict';
-  var FORM_SELECTOR = '[data-coffee-order="form"]';
-  var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
-  //var SERVER_URL = 'http://localhost:3000/coffeeorders';
-  var SERVER_URL = 'https://coffeerun-david810376.firebaseio.com';
-  var App = window.App;
-  var Truck = App.Truck;
-  var DataStore = App.DataStore;
-  var RemoteDataStore = App.RemoteDataStore;
-  var FormHandler = App.FormHandler;
-  var Validation = App.Validation;
-  var CheckList = App.CheckList;
-  var remoteDS = new RemoteDataStore(SERVER_URL);
+(function (window) {
+    'use strict';
+    var FORM_SELECTOR = '[data-coffee-order="form"]';
+    var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';     // CHOOSE ONLY ONE...
+    //var SERVER_URL = 'https://co.audstanley.com/coffeeorders';    // if running on the shared server
+    // var SERVER_URL = 'http://localhost:3000/coffeeorders';          // if running locally
+    var SERVER_URL = 'https://coffeerun-david810376.firebaseio.com';
 
+    var App = window.App;
+    var Truck = App.Truck;
+    var DataStore = App.DataStore;
+    var RemoteDataStore = App.RemoteDataStore;
+    var FormHandler = App.FormHandler;
+    var Validation = App.Validation;
+    var CheckList = App.CheckList;
 
-  //var webshim = window.webshim;
-  var myTruck = new Truck("ncc-1701", remoteDS);
-  window.myTruck = myTruck;
-  var checkList = new CheckList(CHECKLIST_SELECTOR);
-  checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+    var remoteDS = new RemoteDataStore(SERVER_URL);
+    // var truck = new Truck('ncc-1701', new DataStore());
+    var truck = new Truck('ncc-1701', remoteDS);
+    window.truck = truck;
+    var checkList = new CheckList(CHECKLIST_SELECTOR);
+    checkList.addClickHandler(truck.deliverOrder.bind(truck));
+    var formHandler = new FormHandler(FORM_SELECTOR);
 
-  var formHandler = new FormHandler(FORM_SELECTOR);
-  formHandler.addSubmitHandler(function(data) {
-    myTruck.createOrder.call(myTruck, data);
-    checkList.addRow.call(checkList, data);
-  });
+    formHandler.addSubmitHandler(function(data) {
+        return truck.createOrder.call(truck, data)
+            .then(() => {
+                checkList.addRow.call(checkList, data);
+            }); 
+    });
+    console.log(formHandler);
 
-  formHandler.addInputHandler(Validation.isCompanyEmail);
-  //webshim.polyfill('from forms-ext');
-  //webshim.setOptions('from',{addValidators: true, lazyCustomMessages: true});
-
-  remoteDS.getAll();
+    formHandler.addInputHandler(Validation.isCompanyEmail);
+    truck.printOrders(checkList.addRow.bind(checkList));
 
 })(window);
