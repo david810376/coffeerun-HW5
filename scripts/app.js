e320bfeab0741165ebdf184335707e789673f630
david810376 <33507197+david810376@users.noreply.github.com>
2020-10-25 18:21:48 -0700

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -4,14 +4,12 @@ window.FireBaseConfig =
 
 {
 
-    apiKey: "AIzaSyAllNwLGitUjWxJEznyfCfFYp6rbZRW6QY",
-    authDomain: "coffeerun-david810376.firebaseapp.com",
-    databaseURL: "https://coffeerun-david810376.firebaseio.com",
-    projectId: "coffeerun-david810376",
-    storageBucket: "coffeerun-david810376.appspot.com",
+    apiKey: "apiKey",
+    authDomain: "project_ID.firebaseapp.com",
+    databaseURL: "https://project_ID.firebaseio.com",
+    projectId: "project_ID",
+    storageBucket: "project_ID.appspot.com",
     messagingSenderId: "440501069273",
-    appId: "1:440501069273:web:75dfdfd4f5fded5ea025c8",
-    measurementId: "G-LQTYKRME5L"
   };
 
 
