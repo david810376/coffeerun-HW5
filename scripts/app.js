304945e8371633b52259d33089ae966a7b625111
david810376 <33507197+david810376@users.noreply.github.com>
2020-10-25 17:53:48 -0700

    Create app.js

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+(function(window){
+'use strict';
+window.FireBaseConfig = 
+
+{
+
+    apiKey: "AIzaSyAllNwLGitUjWxJEznyfCfFYp6rbZRW6QY",
+    authDomain: "coffeerun-david810376.firebaseapp.com",
+    databaseURL: "https://coffeerun-david810376.firebaseio.com",
+    projectId: "coffeerun-david810376",
+    storageBucket: "coffeerun-david810376.appspot.com",
+    messagingSenderId: "440501069273",
+    appId: "1:440501069273:web:75dfdfd4f5fded5ea025c8",
+    measurementId: "G-LQTYKRME5L"
+  };
+
+
+})(window);
